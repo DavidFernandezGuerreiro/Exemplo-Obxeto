a73aa8d3ec3214b438a9181ca9c70a40bfcd22de
dfernandezguerreiro <dfernandezguerreiro@victoria2.danielcastelao.org>
2017-12-12 08:58:03 +0100

    COMMIT GITHUB.

---
+++ src/exemploobxecto/Semaforo.java
@@ -0,0 +1,19 @@
+
+package exemploobxecto;
+
+/**
+ *
+ * @author dfernandezguerreiro
+ */
+public class Semaforo {
+    //atributos
+    private String cor;
+    //métodos de acceso
+    public void darCor(String c){
+        cor=c;
+    }
+    public String dimeCor(){
+        return cor;
+    }
+    
+}
